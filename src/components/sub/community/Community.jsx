9cf75e112c524aa006f4aeccd29ad306a48960b0
nikji <display1180@gmail.com>
2023-10-29 12:58:59 +0900

    글작성 취소버튼 아이콘연결

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -1,8 +1,30 @@
 import './Community.scss';
 import Layout from '../../common/layout/Layout';
+import { useRef } from 'react';
+import { TfiWrite } from 'react-icons/tfi';
+import { ImCancelCircle } from 'react-icons/im';
 
 function Community() {
-	return <Layout title={'Community'}>Community</Layout>;
+	const refInput = useRef(null);
+	const refTextarea = useRef(null);
+	return (
+		<Layout title={'Community'}>
+			Community
+			<div className='inputBox'>
+				<input type='text' placeholder='title' ref={refInput} />
+				<textarea cols='30' rows='3' placeholder='leave message' ref={refTextarea}></textarea>
+
+				<nav>
+					<button>
+						<ImCancelCircle fontSize={20} color={'#555'} />
+					</button>
+					<button>
+						<TfiWrite fontSize={20} color={'#555'} />
+					</button>
+				</nav>
+			</div>
+		</Layout>
+	);
 }
 
 export default Community;
