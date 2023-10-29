8d7992261e382a8997f49d79c078e5996ae9e667
nikji <display1180@gmail.com>
2023-10-29 13:21:24 +0900

    crud주석정리

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -31,3 +31,18 @@ function Community() {
 }
 
 export default Community;
+
+/*
+  Create (글작성) "POST"
+  Read (글 불러오기) "GET"
+  Updated (글 수정) "PUT"
+  Delete (글 삭제) "DELETE"
+
+  Restful API : DB의 구조적으로 변경하기 위한 개발 방법론
+
+  로컬저장소: LocalStorage 
+  - 모든 브라우저가 내장하고 있는 결량의 저장공간
+  - 문자값만 저장가능 (5MB)
+  - 객체값을 문자화 시켜서 저장
+  - 로컬저장소 값을 불러올때는 반대로 문자형태를 JSON형태로 객체로 parcing해서 가져옴
+*/
