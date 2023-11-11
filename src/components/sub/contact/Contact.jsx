92b23bb167a496726fccf92d0fb96642c2da2cc4
eun-su-jeong <dmswid0@naver.com>
2023-11-11 13:50:46 +0900

    정밀하게 마커 표시하기

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -7,7 +7,10 @@ export default function Contact() {
 	const { kakao } = window;
 
 	const mapOption = {
-		center: new kakao.maps.LatLng(37.5609223, 126.9811389),
+		//위치값 정밀하게 보정하는 법
+		//기존 구글지도 위치값 복사뒤 카카오예제의 클릭한위치 마커표소 직접해보기에서
+		//해당 코드 붙여넣기하고 원하는 지점을 찍으면 아래와같이 정밀한 수치값을 확인가능
+		center: new kakao.maps.LatLng(37.56087805104125, 126.98114395004761),
 		level: 3,
 	};
 	const marker = new kakao.maps.Marker({
