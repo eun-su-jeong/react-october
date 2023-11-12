c1079798d55f83b1a11394923627a09329905a78
eun-su-jeong <dmswid0@naver.com>
2023-11-12 12:57:11 +0900

    지도타입, 줌 컨트롤러 추가

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,7 +1,6 @@
 import { useRef, useEffect, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Contact() {
 	const { kakao } = window;
@@ -37,7 +36,11 @@ export default function Contact() {
 
 	const marker = new kakao.maps.Marker({
 		position: info.current[Index].latlng,
-		image: new kakao.maps.MarkerImage(info.current[Index].imgSrc, info.current[Index].imgSize, info.current[Index].imgPos),
+		image: new kakao.maps.MarkerImage(
+			info.current[Index].imgSrc,
+			info.current[Index].imgSize,
+			info.current[Index].imgPos
+		),
 	});
 
 	const setCenter = () => {
@@ -46,15 +49,30 @@ export default function Contact() {
 
 	useEffect(() => {
 		mapFrame.current.innerHTML = '';
-		mapInstance.current = new kakao.maps.Map(mapFrame.current, { center: info.current[Index].latlng });
+		//지도 인스턴스 생성해서 지도화면 렌더링
+		mapInstance.current = new kakao.maps.Map(mapFrame.current, {
+			center: info.current[Index].latlng,
+		});
+		//지도인스턴스에 맵타입 인스턴스로 타입컨트롤러 추가
+		mapInstance.current.addControl(
+			new kakao.maps.MapTypeControl(),
+			kakao.maps.ControlPosition.TOPRIGHT
+		);
+		//지도인스턴스에 줌 인스턴스로 줌 컨트롤러 추가
+		mapInstance.current.addControl(new kakao.maps.ZoomControl(), kakao.maps.ControlPosition.RIGHT);
+		//마커 인스턴에 맵 인스턴스 결해서 마커 출력
 		marker.setMap(mapInstance.current);
+
+		setTraffic(false);
 
 		window.addEventListener('resize', setCenter);
 	}, [Index]);
 
 	//교통정보 보기 토글 기능
 	useEffect(() => {
-		Traffic ? mapInstance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : mapInstance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+		Traffic
+			? mapInstance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+			: mapInstance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
 
 	useEffect(() => {
@@ -74,7 +92,9 @@ export default function Contact() {
 			</ul>
 
 			<button onClick={setCenter}>위치 초기화</button>
-			<button onClick={() => setTraffic(!Traffic)}>{Traffic ? '교통정보 끄기' : '교통정보 보기'}</button>
+			<button onClick={() => setTraffic(!Traffic)}>
+				{Traffic ? '교통정보 끄기' : '교통정보 보기'}
+			</button>
 		</Layout>
 	);
 }
