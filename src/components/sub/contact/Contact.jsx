74790d9a6a16c84523275047bef456e7f024893b
eun-su-jeong <dmswid0@naver.com>
2023-11-12 15:08:58 +0900

    기본 폼메일 기능 연결

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -50,7 +50,7 @@ export default function Contact() {
 	const sendEmail = (e) => {
 		e.preventDefault();
 
-		emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY').then(
+		emailjs.sendForm('service_uvbnywe', 'template_t0j4wqi', form.current, 'dFZapsiE2-w0j7dXR').then(
 			(result) => {
 				console.log(result.text);
 			},
@@ -104,10 +104,13 @@ export default function Contact() {
 			<div className='mailBox'>
 				<form ref={form} onSubmit={sendEmail}>
 					<label>Name</label>
-					<input type='text' name='user_name' />
+					{/* from_name : 템플릿에서 전송하는 사람이름 변수명 */}
+					<input type='text' name='reply_to' />
 					<label>Email</label>
+					{/* reply_to : 템플릿에서 답장할 메일주소 변수명 */}
 					<input type='email' name='user_email' />
 					<label>Message</label>
+					{/* message : 템플릿에서 문의메세지 변수명 */}
 					<textarea name='message' />
 					<input type='submit' value='Send' />
 				</form>
