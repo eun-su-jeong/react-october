ffce2385cc3e0281a94d07835035dbca95933e24
eun-su-jeong <dmswid0@naver.com>
2023-11-25 14:11:30 +0900

    useThrottle 적용 및 개념 설명

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -7,29 +7,24 @@ function Btns() {
 	const [Num, setNum] = useState(0);
 	const secs = useRef(null);
 	const btns = useRef(null);
-	const eventBoloker = useRef(null);
 
 	const activation = () => {
-		// eventBloker 참조객체 값이 있으면 return로 함수 종료
-		if (eventBoloker.current) return;
+		console.log('activation');
+		const scroll = window.scrollY;
+		secs.current.forEach((el, idx) => {
+			if (scroll >= el.offsetTop - window.innerHeight / 2) {
+				Array.from(btns.current.children).forEach((btn) => btn.classList.remove('on'));
+				btns.current.children[idx]?.classList.add('on');
 
-		//activation함수를 setTimeout을 묶어놓은 다음에 setTimeout이 끝나야지만 eventBlocker값을 비움으로써
-		//강제로 0.5초동안 함수 호출을 막아줌
-		eventBoloker.current = setTimeout(() => {
-			console.log('activation');
-			const scroll = window.scrollY;
-			secs.current.forEach((el, idx) => {
-				if (scroll >= el.offsetTop - window.innerHeight / 2) {
-					Array.from(btns.current.children).forEach((btn) => btn.classList.remove('on'));
-					btns.current.children[idx]?.classList.add('on');
-
-					secs.current.forEach((sec) => sec.classList.remove('on'));
-					secs.current[idx].classList.add('on');
-				}
-			});
-			eventBoloker.current = null;
-		}, 500);
+				secs.current.forEach((sec) => sec.classList.remove('on'));
+				secs.current[idx].classList.add('on');
+			}
+		});
 	};
+
+	//useThrottle커스텀훅의 인수로 activation함수를 전달해서
+	//throttle이 적용된 activation2라는 함수를 반환받음
+	const activation2 = useThrottle(activation);
 
 	const handleClick = (idx) => {
 		new Anime(window, { scroll: secs.current[idx].offsetTop }, { duration: 500 });
@@ -38,10 +33,11 @@ function Btns() {
 	useEffect(() => {
 		secs.current = btns.current.parentElement.querySelectorAll('.myScroll');
 		setNum(secs.current.length);
-		window.addEventListener('scroll', activation);
+		//scroll이벤트는 throttle이 적용된 activation2함수를 연결
+		window.addEventListener('scroll', activation2);
 
 		return () => {
-			window.removeEventListener('scroll', activation);
+			window.removeEventListener('scroll', activation2);
 		};
 	}, []);
 
@@ -50,26 +46,27 @@ function Btns() {
 	return (
 		<ul className='btns' ref={btns}>
 			{Array(Num)
-				.fill()
+				.fill('abc')
 				.map((_, idx) => {
-					return <li key={(_, idx)} onClick={() => handleClick(idx)}></li>;
+					return <li key={_ + idx} onClick={() => handleClick(idx)}></li>;
 				})}
 		</ul>
 	);
 }
 
-// useCallback : 함수자체를 메모이제이션해서 해당 함수를 재활용
-// useMemo : 함수 리턴값 자체를 메모이제이션
-// memo : 컴포넌트 자체를 메모이제이션
-
-// 고차컴포넌트(hoc): high order component
-// 인수를 컴포넌트를 전달 받아서 새로운 컴포넌트를 반환
-
-// hook의 조건
-// 1. 이름이 use로 시작
-// 2. 커스텀 훅은 무조건 함수나 리턴값을 반환
-// 3. 다른 훅이나 핸들러 함수 안쪽에서 호출이 불가, 컴포넌트 함수 안쪽에서만 호출 가능
-
-// throttle :  강제로 인벤트핸들러 호출횟수를 압박해서 줄이는 기법
-// scroll, resize, mousemove, mousewheel : 단기간에 많은 핸들러를 호출하는 이벤트 (1초 60번, 화면주사율 60hz)
 export default Btns;
+
+//useCallback: 함수자체를 메모이제이션해서 해당 함수를 재활용
+//useMemo: 함수의 리턴값 자체를 메모이제이션
+//meno: 컴포넌트 자체를 메모이제이션
+
+//고차컴포넌트(hoc): high order component
+//인수로 컴포넌트를 전달받아서 새로운 컴포넌트를 반환
+
+//hook의 조건
+//1. 이름이 use로 시작
+//2. 커스텀훅은 무조건 함수나 리턴값을 반환
+//3. 다른 hook이나 핸들러함수 안쪽에 호출이 불가, 컴포넌트 함수 안쪽에서만 호출 가능
+
+//throttle : 강제로 이벤트핸들러 호출횟수를 압박해서 줄이는 기법
+//scroll, resize, mousemove, mousewheel : 단기간에 많은 핸들러를 호출하는 이벤트 (1초 60번, 화면주사율 60hz)
