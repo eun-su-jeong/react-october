e3d8a40e594b1a0d6f1d59f26f426fd67ba49da3
eun-su-jeong <dmswid0@naver.com>
2023-12-23 14:10:17 +0900

    비동기 데이터 전역 state관리

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,66 +1,50 @@
-import './Department.scss';
-import Layout from '../../common/layout/Layout';
-import React, { useEffect, useRef, useState } from 'react';
+import "./Department.scss";
+import React from "react";
+import Layout from "../../common/layout/Layout";
+import { useSelector } from "react-redux";
 
 export default function Department() {
-	const [Department, setDepartment] = useState([]);
-	const [History, setHistory] = useState([]);
-	const path = useRef(process.env.PUBLIC_URL);
+  const path = process.env.PUBLIC_URL;
+  const Department = useSelector((store) => store.departmentReducer.department);
+  const History = useSelector((store) => store.historyReducer.history);
 
-	const fetchDepartment = async () => {
-		const data = await fetch(`${path.current}/DB/history.json`);
-		const json = await data.json();
-		setHistory(json.history);
-	};
+  return (
+    <Layout title={"Department"}>
+      <section id="historyBox">
+        <h2>History</h2>
+        <div className="con">
+          {History.map((data, idx) => {
+            return (
+              <React.Fragment key={idx}>
+                <h3>{Object.keys(data)[0]}</h3>
+                <ul>
+                  {Object.values(data)[0].map((val, idx) => (
+                    <li key={idx}>{val}</li>
+                  ))}
+                </ul>
+              </React.Fragment>
+            );
+          })}
+        </div>
+      </section>
 
-	const fetchHistory = async () => {
-		const data = await fetch(`${path.current}/DB/department.json`);
-		const json = await data.json();
-		setDepartment(json.members);
-	};
+      <section id="memberBox">
+        <h2>Department</h2>
 
-	useEffect(() => {
-		fetchDepartment();
-		fetchHistory();
-	}, []);
-
-	return (
-		<Layout title={'Department'}>
-			<section id='historyBox'>
-				<h2>History</h2>
-				<div className='con'>
-					{History.map((data, idx) => {
-						return (
-							<React.Fragment key={idx}>
-								<h3>{Object.keys(data)[0]}</h3>
-								<ul>
-									{Object.values(data)[0].map((val, idx) => (
-										<li key={idx}>{val}</li>
-									))}
-								</ul>
-							</React.Fragment>
-						);
-					})}
-				</div>
-			</section>
-
-			<section id='memberBox'>
-				<h2>Department</h2>
-
-				<div className='con'>
-					{Department.map((member, idx) => {
-						return (
-							<article key={idx}>
-								<div className='pic'>
-									<img src={`${path.current}/img/${member.pic}`} alt={member.name} />
-								</div>
-								<h3>{member.name}</h3>
-								<p>{member.position}</p>
-							</article>
-						);
-					})}
-				</div>
-			</section>
-		</Layout>
-	);
+        <div className="con">
+          {Department.map((member, idx) => {
+            return (
+              <article key={idx}>
+                <div className="pic">
+                  <img src={`${path}/img/${member.pic}`} alt={member.name} />
+                </div>
+                <h3>{member.name}</h3>
+                <p>{member.position}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    </Layout>
+  );
 }
