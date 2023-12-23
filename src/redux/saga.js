7e7e515cd9123f53e48176339427516d15b9d6b9
eun-su-jeong <dmswid0@naver.com>
2023-12-23 14:07:43 +0900

    department, histroy 데이터 전역 state연동

--- src/redux/saga.js
+++ src/redux/saga.js
@@ -1,5 +1,10 @@
 import { takeLatest, all, put, fork, call } from "redux-saga/effects";
-import { fetchFlickr, fetchYoutube } from "./api";
+import {
+  fetchFlickr,
+  fetchYoutube,
+  fetchDepartment,
+  fetchHistory,
+} from "./api";
 import * as types from "./actionType";
 
 function* callFlickr() {
@@ -17,7 +22,7 @@ function* callFlickr() {
 }
 
 function* callYoutube() {
-  yield takeLatest(types.YOUTUBE.start, function* (action) {
+  yield takeLatest(types.YOUTUBE.start, function* () {
     try {
       const response = yield call(fetchYoutube);
       yield put({
@@ -30,6 +35,39 @@ function* callYoutube() {
   });
 }
 
+function* callDepartment() {
+  yield takeLatest(types.DEPARTMENT.start, function* () {
+    try {
+      const response = yield call(fetchDepartment);
+      yield put({
+        type: types.DEPARTMENT.success,
+        payload: response.members,
+      });
+    } catch (err) {
+      yield put({ type: types.DEPARTMENT.fail, payload: err });
+    }
+  });
+}
+
+function* callHistory() {
+  yield takeLatest(types.HISTORY.start, function* () {
+    try {
+      const response = yield call(fetchHistory);
+      yield put({
+        type: types.HISTORY.success,
+        payload: response.history,
+      });
+    } catch (err) {
+      yield put({ type: types.HISTORY.fail, payload: err });
+    }
+  });
+}
+
 export default function* rootSaga() {
-  yield all([fork(callFlickr), fork(callYoutube)]);
+  yield all([
+    fork(callFlickr),
+    fork(callYoutube),
+    fork(callDepartment),
+    fork(callHistory),
+  ]);
 }
