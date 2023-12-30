8168493c5917126728de2e176929c4fd6081fc27
eun-su-jeong <dmswid0@naver.com>
2023-12-30 13:48:07 +0900

    react-query 커스텀훅으로 간편하게 서버 데이터 호출 및 관리

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,66 +1,66 @@
-import './Department.scss';
-import Layout from '../../common/layout/Layout';
-import React, { useEffect, useRef, useState } from 'react';
+import "./Department.scss";
+import Layout from "../../common/layout/Layout";
+import React, { useEffect, useState } from "react";
+import { useMembersQuery } from "../../../hooks/useMembersQuery";
+
+const path = process.env.PUBLIC_URL;
 
 export default function Department() {
-	const [Department, setDepartment] = useState([]);
-	const [History, setHistory] = useState([]);
-	const path = useRef(process.env.PUBLIC_URL);
+  const [History, setHistory] = useState([]);
+  const { isSuccess, data: Department } = useMembersQuery();
 
-	const fetchDepartment = async () => {
-		const data = await fetch(`${path.current}/DB/history.json`);
-		const json = await data.json();
-		setHistory(json.history);
-	};
+  const fetchHistory = async () => {
+    const data = await fetch(`${path}/DB/history.json`);
+    const json = await data.json();
 
-	const fetchHistory = async () => {
-		const data = await fetch(`${path.current}/DB/department.json`);
-		const json = await data.json();
-		setDepartment(json.members);
-	};
+    setHistory(json.history);
+  };
 
-	useEffect(() => {
-		fetchDepartment();
-		fetchHistory();
-	}, []);
+  useEffect(() => {
+    fetchHistory();
+  }, []);
 
-	return (
-		<Layout title={'Department'}>
-			<section id='historyBox'>
-				<h2>History</h2>
-				<div className='con'>
-					{History.map((data, idx) => {
-						return (
-							<React.Fragment key={idx}>
-								<h3>{Object.keys(data)[0]}</h3>
-								<ul>
-									{Object.values(data)[0].map((val, idx) => (
-										<li key={idx}>{val}</li>
-									))}
-								</ul>
-							</React.Fragment>
-						);
-					})}
-				</div>
-			</section>
+  return (
+    <Layout title={"Department"}>
+      <section id="historyBox">
+        <h2>History</h2>
+        <div className="con">
+          {History.map((data, idx) => {
+            return (
+              <React.Fragment key={idx}>
+                <h3>{Object.keys(data)[0]}</h3>
+                <ul>
+                  {Object.values(data)[0].map((val, idx) => (
+                    <li key={idx}>{val}</li>
+                  ))}
+                </ul>
+              </React.Fragment>
+            );
+          })}
+        </div>
+      </section>
 
-			<section id='memberBox'>
-				<h2>Department</h2>
+      <section id="memberBox">
+        <h2>Department</h2>
 
-				<div className='con'>
-					{Department.map((member, idx) => {
-						return (
-							<article key={idx}>
-								<div className='pic'>
-									<img src={`${path.current}/img/${member.pic}`} alt={member.name} />
-								</div>
-								<h3>{member.name}</h3>
-								<p>{member.position}</p>
-							</article>
-						);
-					})}
-				</div>
-			</section>
-		</Layout>
-	);
+        <div className="con">
+          {isSuccess ? (
+            Department.map((member, idx) => {
+              return (
+                <article key={idx}>
+                  <div className="pic">
+                    <img src={`${path}/img/${member.pic}`} alt={member.name} />
+                  </div>
+                  <h3>{member.name}</h3>
+                  <p>{member.position}</p>
+                </article>
+              );
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </section>
+    </Layout>
+  );
 }
